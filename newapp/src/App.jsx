868b8b4dd672453ba759a7e19c48f18f98a4fa1a
NIVEDH SUNIL <nishikradha@gmail.com>
2026-02-10 11:06:32 +0530

    Update App.jsx

--- newapp/src/App.jsx
+++ newapp/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { use } from 'react'
+import { useState } from 'react'
 export default function App() {
-  const userName = "Vijay"
+  const [userName] = useState{"Vijay"}
   return (
     <div>
       <h1>Happy New year {userName} </h1>
